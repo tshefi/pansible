b11d53b6ce15bd4b8d5d03ba1df1d19d3c9d5fdc
tshefi <tshefi@redhat.com>
2017-07-20 15:06:42 +0300

    Add support for Swift/Ceph/Cinder nodes

--- pansible.sh
+++ pansible.sh
@@ -8,16 +8,27 @@ user="ansible_user=heat-admin"
 sudo yum install -y -q -e 0 crudini ansible
 nova list | awk '{print $4 "\t" $12}' | grep co > output.txt &&  sed -i s/ctlplane=//g output.txt
 
-echo "[controllers]" >> inventory
+echo "[controller]" >> inventory
 #if ! grep -q controllers inventory; then
 #     echo "[controllers]" >> inventory
 #fi
 for i in $(grep controller output.txt | awk '{print $2}'); do echo $i$user >> inventory ; done
-echo "[computes]" >> inventory
+
+echo "[compute]" >> inventory
 for i in $(grep compute output.txt | awk '{print $2}'); do echo $i$user >> inventory ; done
 
+echo "[swift-storage]" >> inventory
+for i in $(grep "swift-storage" output.txt | awk '{print $2}'); do echo $i$user >> inventory ; done
+
+echo "[ceph-storage]" >> inventory
+for i in $(grep "ceph-storage" output.txt | awk '{print $2}'); do echo $i$user >> inventory ; done
+
+echo "[block-storage]" >> inventory
+for i in $(grep "block-storage" output.txt | awk '{print $2}'); do echo $i$user >> inventory ; done
+
+
 #cleanup
-rm output.txt
+#rm output.txt
 
 # Show ansible inventory groups
 ansible localhost -i inventory -m debug -a 'var=groups'
