14f7bfbac6c70b2c5944758d0e2e7b3283242743
tshefi <tshefi@redhat.com>
2017-10-25 13:51:42 +0300

    Add logic to set correct default_floating_pool's name Nova or Public on preflight.yml

--- pansible.sh
+++ pansible.sh
@@ -64,6 +64,14 @@ if [ ! -f ./preflight.yml ]; then
   wget https://raw.githubusercontent.com/tshefi/pansible/master/preflight.yml
 fi
 
+sudo grep default_floating_pool=nova /etc/nova/nova.conf
+if [ $? == 0 ]; then
+   echo "Default_floating_pool=nova"
+   sed -i s/NetName/nova/g /home/stack/preflight.yml
+else
+   echo  "Default_floating_pool=public"
+   sed -i s/NetName/public/g /home/stack/preflight.yml
+fi
 # Source overcloud
 . $OVERRC
 echo "Sourced overcloudrc, and run playbook."
